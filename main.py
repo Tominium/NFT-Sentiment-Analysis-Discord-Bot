0bd6d17940951e0d074f68dbb72ed1274e373c9b
Tomin <tchazhikat@gmail.com>
2022-07-03 19:28:32 -0500

    Added Google sentiment analysis

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ import discord
 
 ##text = input("Enter Name of NFT Project To Analyze: ")
 
+analyzer = SentimentIntensityAnalyzer()
+
+new_words = {
+    'wgmi': 4,
+    'ngmi': -4,
+    '🔥' : 4,
+    'hacked' : -1,
+    'rug': -2,
+    'was rugged': -4,
+    'wagmi': 4
+}
+analyzer.lexicon.update(new_words)
+
 def get_sentiment(text):
     tweets = []
     twitter = Twitter(language='en')
@@ -15,26 +28,32 @@ def get_sentiment(text):
     tweets =  [re.sub(r'@[A-Za-z0-9]+','', str(x)) for x in tweets]
     tweets =  [re.sub(r'^https?:\/\/.*[\r\n]*','', str(x)) for x in tweets]
 
-    analyzer = SentimentIntensityAnalyzer()
-
-    new_words = {
-        'wgmi': 4,
-        'ngmi': -4,
-        '🔥' : 4,
-        'hacked' : -1,
-        'rug': -2,
-        'was rugged': -4
-    }
-    analyzer.lexicon.update(new_words)
-
-
     tweet_score = analyzer.polarity_scores((tweets))
     tw_df = pd.DataFrame(tweet_score, index=[0])
 
+    goog = []
+    engine = Google()
+    for result in engine.search(text + "NFT", cached=False, count=25):
+        test = result.text.replace('<b>', '')
+        test = test.replace('</b>', '')
+        goog.append(test)
+
+    goog_score = analyzer.polarity_scores((goog))
+    goog_df = pd.DataFrame(goog_score, index=[0])
+
     tweet_mean = (tw_df['compound'].mean())
+    goog_mean = (goog_df['compound'].mean())
+
+    tweet_mean = (tweet_mean * 0.85) + (goog_mean * 0.15)
+
     if(tweet_mean == 0):
         return "Not Valid Search"
-    return tweet_mean
+
+    if(tweet_mean > 0.9):
+        return "🔥 Hot 🔥\n Score: " + str(tweet_mean)
+    elif(tweet_mean < 0.9 and tweet_mean > 0.7):
+        return "Mid \n Score: " + str(tweet_mean)
+    return "🗑️ Trash 🗑️\n Score: " + str(tweet_mean)
 
 client = discord.Client()
 
@@ -52,6 +71,6 @@ async def on_message(message):
         await message.channel.send(get_sentiment(text))
     
     if message.content.startswith('!help'):
-        await message.channel.send("> `Command`: `!search <NFT Project Name>`")
+        await message.channel.send("> Scores go `-1.0` - `1.0` (With 1 being most positive and -1 being the most negative) \n > `Command`: `!search <NFT Project Name>`")
 
-client.run('OTkzMjk2MTE2ODczNDI5MTEy.GJkbG6.IUEKvS0axE7tU9FrubtFwasO6hicD1lKwOoU-A')
+client.run('OTkzMjk2MTE2ODczNDI5MTEy.GBmncs._xxB4kknrGOWaL2TrAdSENzTGrPIZIT2voCvZ8')
